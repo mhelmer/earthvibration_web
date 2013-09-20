d2c4cad5b9c8b20597c4a84889ec2dc0e6b2c60b
Magnus Helmersson <magnus.helmersson@gmail.com>
2013-09-20 11:20:42 +0200

    Add an event mdel

--- events/models.py
+++ events/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=150)
+    date = models.DateTimeField('event date')
+    description = models.TextField()
+    location = models.CharField(max_length=200)
+    admission = models.CharField(max_length=20)
+    published = models.BooleanField()
+
+    def __unicode__(self):
+        return self.name
+
