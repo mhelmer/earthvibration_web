a23756e27913103dff1b32f70f8a53809343fe6c
Magnus Helmersson <magnus.helmersson@gmail.com>
2013-10-04 18:53:07 +0200

    events: Add age limit, flyer and update admin page

--- events/models.py
+++ events/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+import os
+
+
+def get_image_path(instance, filename):
+    return os.path.join('events', str(instance.id), 'flyer', filename)
 
 
 class Event(models.Model):
@@ -7,8 +12,14 @@ class Event(models.Model):
     description = models.TextField()
     location = models.CharField(max_length=200)
     admission = models.CharField(max_length=20)
+    ageLimit = models.IntegerField(null=True)
     published = models.BooleanField()
 
-    def __unicode__(self):
-        return self.name
+    flyer = models.ImageField(upload_to=get_image_path, null=True,
+                              blank=True)
 
+    def flyer_tag(self):
+        return u'<img src="%s" />' % self.flyer.url
+
+    flyer_tag.short_description = 'Flyer'
+    flyer_tag.allow_tags = True
