59084e37c3b222cd2358d5571bf23ce4231bda21
Magnus Helmersson <magnus.helmersson@gmail.com>
2013-10-04 12:53:55 +0200

    Set STATICDIR to ../static.sitename

--- earthvibration/settings.py
+++ earthvibration/settings.py
@@ -64,7 +64,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(PROJECT_DIR)),
+                           "static." + os.path.basename(PROJECT_DIR))
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
