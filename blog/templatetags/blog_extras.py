45865532849b6dec3b8df148f4f152ca4fee3fcc
Magnus Helmersson <magnus.helmersson@gmail.com>
2013-10-07 11:29:09 +0200

    blog: Refactor and use templatetag for details

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -12,6 +12,15 @@ def list_tags(blog_entry):
     return {'tags': blog_entry.tags.all()}
 
 
+def show_blog_entry(blog_entry):
+    return {'blog_entry': blog_entry}
+
+
+def show_blog_entry_details(blog_entry):
+    return {'blog_entry': blog_entry}
+
 register.simple_tag(get_tag_url)
 register.inclusion_tag('blog/list_tags.html')(list_tags)
-
+register.inclusion_tag('blog/show_entry.html')(show_blog_entry)
+register.inclusion_tag('blog/show_entry_details.html')(
+    show_blog_entry_details)
