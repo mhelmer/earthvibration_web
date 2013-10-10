8e4f139de53b2eb13fcb8567e68586bf2ec12c00
Magnus Helmersson <magnus.helmersson@gmail.com>
2013-10-10 11:04:24 +0200

    blog: Admin sets the times, author set on creation

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,21 @@ from django.contrib import admin
 from blog.models import BlogEntry
 
 
-admin.site.register(BlogEntry)
+class BlogEntryAdmin (admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['title', 'slug', 'date_published',
+                           'date_modified', 'content', 'author', 'tags']}),
+    ]
+    list_display = ('title', 'date_published', 'date_modified', 'slug',
+                    'author')
+    list_filter = ['date_published']
+    search_fields = ['title', 'content']
+    date_hierachy = 'date_published'
+    readonly_fields = ('date_published', 'date_modified',)
+
+    def save_model(self, request, obj, form, change):
+        if not self.pk:
+            obj.author = request.user
+        obj.save()
+
+admin.site.register(BlogEntry, BlogEntryAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+
 from taggit.managers import TaggableManager
 
 
 class BlogEntry(models.Model):
     title = models.CharField(max_length=150)
     slug = models.SlugField('slug', unique_for_date='date_published')
-    date_published = models.DateTimeField('date published')
-    date_modified = models.DateTimeField('last modified')
+    date_published = models.DateTimeField('date publsihed', auto_now_add=True)
+    date_modified = models.DateTimeField('last modified', auto_now=True)
     content = models.TextField()
-    author = models.CharField(max_length=100)
+    author = models.ForeignKey(User)
 
     tags = TaggableManager()
 
