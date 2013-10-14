fddb3372e4ca13a6d0e5101aac0f3af9c8085181
Magnus Helmersson <magnus.helmersson@gmail.com>
2013-10-14 14:39:03 +0200

    home: Should only accept url/ and not url/*

--- home/urls.py
+++ home/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, include, url
 from home import views
 
 urlpatterns = patterns('',
-    url(r'^soundsystem/', views.SoundsystemView.as_view(), name="soundsystem"),
-    url(r'^studio/', views.StudioView.as_view(), name="studio"),
-    url(r'^releases/', views.ReleasesView.as_view(), name="releases"),
-    url(r'^contact/', views.ContactView.as_view(), name="contact"),
+    url(r'^soundsystem/$', views.SoundsystemView.as_view(), name="soundsystem"),
+    url(r'^studio/$', views.StudioView.as_view(), name="studio"),
+    url(r'^releases/$', views.ReleasesView.as_view(), name="releases"),
+    url(r'^contact/$', views.ContactView.as_view(), name="contact"),
 )
