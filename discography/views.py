050ea4213af2b41ae692d6a8ce153b68a87dc950
Magnus Helmersson <magnus.helmersson@gmail.com>
2013-10-14 12:34:34 +0200

    discography: Index view grouped by year

--- discography/views.py
+++ discography/views.py
@@ -10,7 +10,7 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return Release.objects.filter(
             published=True,
-        )
+        ).order_by('-release_date')
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
